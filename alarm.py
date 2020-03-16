5171bc3087149e783efcdfb9c43374db2d280888
mohammed+1-at-962383805689 <mohammed+1-at-962383805689>
2020-03-16 12:39:23 +0400

    first commit

---
+++ alarm.py
@@ -0,0 +1,28 @@
+"""
+Author : Mulla Mohammed
+Description : This program demonstrates the working on stop watch,
+
+Input : Ask the user in how much time should it buzz
+output : buzz
+working : takes the inputs from user , trigger the buzzer when its about time
+"""
+
+import time, os, winsound
+def buzzer():
+    for i in range(3): #number of repeats
+        for j in range(9): #number of beeps
+            winsound.MessageBeep(-1)
+            time.sleep(1)
+
+def snooze(unit_sec):
+    print("wait time in seconds :", unit_sec)
+    time.sleep(unit_sec)
+    buzzer()
+
+if __name__ == '__main__':
+    print("WELCOME TO STOP WATCH PROGRAM !")
+    minutes = int(input("Please enter Minutes :"))
+    sec = int(input("Please enter seconds:"))
+    minutes = minutes * 60
+    unit_sec = minutes + sec
+    snooze(unit_sec)
